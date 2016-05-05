5b9965a6e037552fc8e4a5b765fdc79339df73ca
pmontenegro <pmontenegro@muxi.com.br>
2016-05-05 14:40:01 -0300

    Troca fluxo para multiplas funcoes

--- autotasker.js
+++ autotasker.js
@@ -26,61 +26,39 @@ var output = "";
 for (var i = 1; i < horarios.length - 1; i++)
 {
 	if (task[i] != "000000")
-	{		
-		output += "function func" + i +"(){";
+	{
+		output += "function func" + i +"part1(){";
 		output += "document.getElementById('btnAddAlocacao-btnWrap').click();";
-		output += "var cont = null;";
-		output += "while(cont == null)";
-		output += "{";
-		output += "setTimeout(function(){}, 50);";
-		output += "cont = document.getElementById('ESFDATA-inputEl');";
-		output += "if (cont != null)";
-		output += "part2();";
+		output += "window.setTimeout(func" + i +"part2, 2000);";
 		output += "}";
-		output += "function part2(){";
+		output += "function func" + i +"part2(){";
 		output += "document.getElementById('ESFDATA-inputEl').value = '" + sData + "';";
 		output += "document.getElementById('ESFHORARIOINICIO-inputEl').value = '" + horarios[i] + "';";
 		output += "document.getElementById('ESFHORARIOTERMINO-inputEl').value = '" + horarios[i-1] + "';";
 		output += "document.getElementById('cmp_ESFSOLCOD-inputEl').value = '" + task[i] + "';";
 		output += "document.getElementById('ESFCOMENTARIO').value = '" + ativs[i] + "';";
-		output += "window.setTimeout(part3, 100);";
+		output += "window.setTimeout(func" + i +"part3, 300);";
 		output += "}";
-		output += "function part3(){";
+		output += "function func" + i +"part3(){";
 		output += "var btnId = document.getElementById('cmp_ESFSOLCOD-triggerWrap').getElementsByTagName('img')[1].id;";
 		output += "document.getElementById(btnId).click();";
-		output += "var cont = document.getElementById('cmp_ESFSOLCOD-inputEl').value;";
-		output += "while(cont.length < 8)";
-		output += "{";
-		output += "setTimeout(function(){}, 50);";
-		output += "cont = document.getElementById('cmp_ESFSOLCOD-inputEl').value;";
-		output += "if (cont.length > 8)";
-		output += "part4();";
+		output += "window.setTimeout(func" + i +"part4, 4000);";
 		output += "}";
-		output += "}";
-		output += "function part4(){";
+		output += "function func" + i +"part4(){";
 		output += "document.getElementById('btnConfirm_win-detail-mdlEsforco-btnIconEl').click();";
-		output += "var cont = null;";
-		output += "while(cont != null)";
-		output += "{";
-		output += "setTimeout(function(){}, 50);";
-		output += "cont = document.getElementById('ESFDATA-inputEl');";
-		output += "if (cont == null)";
-		output += "func" + (i + 1) +"();";
-		output += "}";
-		//output += "window.setTimeout(func" + (i + 1) +", 5000);";
-		output += "}";
+		output += "window.setTimeout(func" + (i + 1) +"part1, 5000);";
 		output += "}";
 	}
 	else
 	{
-		output += "function func" + i +"(){";
-		output += "func" + (i + 1) + "();";
+		output += "function func" + i +"part1(){";
+		output += "func" + (i + 1) + "part1();";
 		output += "}";
 	}
 	
 	if (i + 1 == horarios.length - 1)
 	{
-		output += "function func" + (i + 1) + "(){";
+		output += "function func" + (i + 1) + "part1(){";
 		output += "window.alert('Terminado!');";
 		output += "}";
 	}	
