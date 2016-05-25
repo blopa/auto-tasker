762d0a7ec66b6e356deb56c23c6904ca410dacf6
pmontenegro <pmontenegro@muxi.com.br>
2016-05-25 12:51:43 -0300

    Altera modelo do atividades.html e adiciona console.log no autotasker.js

--- autotasker.js
+++ autotasker.js
@@ -1,6 +1,3 @@
-// TODO fazer while invalid
-
-//var doc = document.getElementsByTagName('pre')[0].firstChild.data;
 var doc = document.body.innerHTML;
 doc = doc.slice(0, doc.indexOf("--------------------")).replace(/^\s*\n/gm,"");
 var atividades = doc.split("\n");
@@ -9,10 +6,12 @@ var isFirst = true;
 var horarios = new Array();
 var ativs = new Array();
 var task = new Array();
+var output = "";
+
 atividades.forEach(function(entry)
 {
 	if (isFirst)
-		isFirst = false;
+		isFirst = false; // pula primeira iteracao
 	else
 	{
 		horarios.push(entry.substring(0, 5));
@@ -21,19 +20,20 @@ atividades.forEach(function(entry)
 	}
 });
 
-var output = "";
 
 for (var i = 1; i < horarios.length - 1; i++)
 {
-	if (task[i] != "000000")
+	if (task[i] != "000000") // task "000000" eh usada para almoco e horario de sair
 	{
 		output += "function func" + i +"part1(){";
+		output += "console.log('Entrou na func" + i +"part1()');"; // console.log part1
 		output += "document.getElementById('btnAddAlocacao-btnWrap').click();";
 		output += "var cont = document.getElementById('ESFCOMENTARIO');";
 		output += "if (cont != null)func" + i +"part1();";
 		output += "else setTimeout(func" + i +"part2, 1000);";
 		output += "}";
 		output += "function func" + i +"part2(){";
+		output += "console.log('Entrou na func" + i +"part2()');"; // console.log part2
 		output += "document.getElementById('ESFDATA-inputEl').value = '" + sData + "';";
 		output += "document.getElementById('ESFHORARIOINICIO-inputEl').value = '" + horarios[i] + "';";
 		output += "document.getElementById('ESFHORARIOTERMINO-inputEl').value = '" + horarios[i-1] + "';";
@@ -42,6 +42,7 @@ for (var i = 1; i < horarios.length - 1; i++)
 		output += "setTimeout(func" + i +"part3, 300);";
 		output += "}";
 		output += "function func" + i +"part3(){";
+		output += "console.log('Entrou na func" + i +"part3()');"; // console.log part3
 		output += "var btnId = document.getElementById('cmp_ESFSOLCOD-triggerWrap').getElementsByTagName('img')[1].id;";
 		output += "var cont = document.getElementById('cmp_ESFSOLCOD-inputEl');";
 		output += "if (!cont.disabled && cont.value.length < 8)document.getElementById(btnId).click();";
@@ -49,6 +50,7 @@ for (var i = 1; i < horarios.length - 1; i++)
 		output += "else setTimeout(func" + i +"part3, 1000);";
 		output += "}";
 		output += "function func" + i +"part4(){";
+		output += "console.log('Entrou na func" + i +"part4()');"; // console.log part4
 		output += "var click = document.getElementById('btnConfirm_win-detail-mdlEsforco-btnIconEl');";
 		output += "if(click != null) click.click();";
 		output += "var apagarbtn = document.getElementById('btnConfirm_win-detail-mdlEsforco');";
@@ -69,6 +71,7 @@ for (var i = 1; i < horarios.length - 1; i++)
 	{
 		output += "function func" + (i + 1) + "part1(){";
 		output += "window.alert('Terminado!');";
+		output += "console.log('Acabou!');"; // console.log acaba
 		output += "}";
 	}
 }
